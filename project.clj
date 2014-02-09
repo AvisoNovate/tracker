165b28bee2efa303a80dd3866b82ec8a271fa648
Howard M. Lewis Ship <hlship@gmail.com>
2014-02-09 10:47:24 -0500

    Initial import

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject io.aviso/tracker "0.1.0"
+  :description "Track per-thread operations when exceptions occur"
+  :url "https://github.com/AvisoNovate/tracker"
+  :license {:name "Apache Sofware Licencse 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 ]
+  :codox {:src-dir-uri               "https://github.com/AvisoNovate/tracker/blob/master/"
+          :src-linenum-anchor-prefix "L"}
+  :profiles {:dev {:dependencies [[log4j "1.2.17"]]}})
