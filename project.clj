5393f08a003fea83673413e1e9de9e6998a37616
Howard M. Lewis Ship <hlship@gmail.com>
2014-08-29 14:57:15 -0700

    Advance dependencies and advance version to 0.1.1

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/AvisoNovate/tracker"
   :license {:name "Apache Sofware Licencse 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [io.aviso/pretty "0.1.8"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [io.aviso/pretty "0.1.12"]]
   :codox {:src-dir-uri               "https://github.com/AvisoNovate/tracker/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:dependencies [[log4j "1.2.17"]]}})
