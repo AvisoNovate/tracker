eae65794f5c53db9a16c80af19f28c6d52e15d8e
Howard M. Lewis Ship <hlship@gmail.com>
2015-02-10 09:22:09 -0800

    Refactor out part of track* function as checkpoint macro and checkpoint* function
    Improve documentation

--- src/io/aviso/tracker.clj
+++ src/io/aviso/tracker.clj
@@ -1,12 +1,14 @@
 (ns io.aviso.tracker
-  "Code for tracking operations so that a history can be presented when an exception is thrown."
+  "Code for tracking operations so that a history can be presented when an exception is thrown.
+
+  The main entrypoint is the [[track]] macro."
   (:require [clojure.string :as str]
             [clojure.tools.logging :as l]
             [clojure.tools.logging.impl :as impl]
             [io.aviso.writer :as writer]
             [io.aviso.exception :as exception]
             [io.aviso.toolchest.exceptions :refer [to-message]])
-  (:import (java.util WeakHashMap Map)))
+  (:import [java.util WeakHashMap Map]))
 
 ;;; Contains a vector of messages (or functions that return messages) used to log the route to the exception.
 (def ^:private ^:dynamic *operation-labels*
@@ -18,6 +20,11 @@
   "Controls whether operation labels are logged on entry to the [[track]] macro. By default (nil), no
   logging takes places.  Otherwise, this is a logging level (such as :debug or :info) to be used."
   nil)
+
+(defn get-logger
+  "Determines the logger instance for a namespace (by leveraging some clojure.tools.logging internals)."
+  [namespace]
+  (impl/get-logger l/*logger-factory* namespace))
 
 (def ^:private ^Map log-exceptions?
   (WeakHashMap.))
@@ -35,7 +42,6 @@
     (.get log-exceptions? (Thread/currentThread))))
 
 (defn- label-to-string
-  "Converts a trace to a string; a trace may be a function which is invoked, a string, or any object that can be converted to a string."
   [message]
   (str (if (fn? message) (message) message)))
 
@@ -59,15 +65,59 @@
          (str/join writer/eol)
          (error logger))))
 
+(defn checkpoint*
+  "The main implementation of [[checkpoint]], using a specific logger.
+
+  :logger
+  : logger to use when logging the operation trace.
+
+  :f
+  : function to evaluate.
+
+  The result of the function is returned.
+
+  If the function throws an exception, it may be logged (and rethrown).  When the exception is logged,
+  it includes the operation trace (the sequence of active operation labels), then the formatted exception.
+
+  In the logging case, the exception is rethrown, wrapped as a new ex-info exception, with :operation-trace
+  as the vector of operation label strings."
+  {:added "0.1.5"}
+  [logger f]
+  (try
+    (set-should-log-operations! true)
+    (f)
+    (catch Throwable t
+      (if (should-log-operations?)
+        (let [trace-label-strings (operation-labels)]
+          (log-trace-label-stack logger trace-label-strings t)
+          (set-should-log-operations! false)
+          (throw (ex-info (to-message t)
+                          {:operation-trace trace-label-strings}
+                          t)))
+        (throw t)))))
+
+(defmacro checkpoint
+  "Establishes a checkpoint, a point at which a thrown exception results in
+  logging of the operation trace.
+
+  checkpoint is useful when flow of control passes from one thread to another, such as
+  when using clojure.core.async. Although the operation trace, being stored in a dynamic var,
+  will be conveyed to the new thread, the exception handler necessary to report an exception
+  is not. checkpoint provides that exception handler."
+  {:added "0.1.5"}
+  [& body]
+  `(checkpoint* (get-logger ~*ns*) (fn [] ~@body)))
+
 (defn track*
-  "Tracks the execution of a function of no arguments. The trace macro converts into a call to track*.
+  "Tracks the execution of a function of no arguments. The [[track]] macro converts into a call to track*.
+  Uses [[checkpoint]].
   
   logger
   : Logger where logging should occur (as defined by the clojure.tools.logging Logger protocol)
 
   label
   : String, object, or function. If a function, the function will only be evaluated if the message
-    needs to be printed (when an exception occurs, or when logging traces via [[*log-traces*]]).
+    needs to be printed (when an exception occurs, or when logging traces via [[*log-trace-level*]]).
 
   f
   : function to invoke. track* returns the result from this function."
@@ -81,41 +131,35 @@
                  (label-to-string label)
                  label)]
     (binding [*operation-labels* (conj *operation-labels* label')]
-      (set-should-log-operations! true)
-      (try
-        (when level'
-          (l/log* logger *log-trace-level* nil label'))
-        (f)
-        (catch Throwable t
-          (if (should-log-operations?)
-            (let [trace-label-strings (operation-labels)]
-              (log-trace-label-stack logger trace-label-strings t)
-              (set-should-log-operations! false)
-              (throw (ex-info (to-message t)
-                              {:operation-trace trace-label-strings}
-                              t)))
-            (throw t)))))))
-
-(defn get-logger
-  "Determines the logger instance for a namespace (by leveraging some clojure.tools.logging internals)."
-  [namespace]
-  (impl/get-logger l/*logger-factory* namespace))
+      (when level'
+        (l/log* logger *log-trace-level* nil label'))
+      (checkpoint* logger f))))
 
 (defmacro track
   "Tracks the execution of an operation, associating the label (which describes the operation) with the execution of the body.
 
-  If an exception occurs inside the body, then all labels leading up to the
-  point of the exception will be logged (the logger is determined from the current namespace); thus logging only occurs at the
-  most deeply nested label. The exception thrown is formatted and logged as well.  The actual exception will be rethrown, wrapped in a
-  new `ex-info` exception, with key `:operation-trace` set to the vector of operation trace strings.
+  track is intended to be nested; each block of work that does something interesting can be wrapped as a track block.
 
-  label may be a string, an object, or a function that returns a string; execution of the function is deferred until
-  an operation trace label is needed.
+  track is particularly useful when operations span across multiple threads.
+
+  track does *not* do anything magic with respect to lazy evaluation.
+
+  The provided label may be:
+
+  * a simple string
+  * a no-arguments function which is invoked, returning a string
+  * any other object, which is converted to a string
+
+  If an exception occurs inside the body, then all operation labels leading up to the
+  point of the exception will be logged. See [[checkpoint*]] for the details.
 
   The operation trace is stored in a dynamic var; if you make use of Clojure's bound-fn macro to execute code in a new thread
   with the same bindings, then operations in a new thread will log as successors to operations in the current thread.
 
-  Note that the clojure.core.async thead and go macros *do* propogate bindings to new threads."
+  Note that the clojure.core.async thread and go macros *do* propogate bindings to new threads, but it will be necessary
+  to use the [[checkpoint]] macro to get the full benefit of operation tracking.
+
+  See also: [[*log-trace-level*]]."
   [label & body]
   `(track* (get-logger ~*ns*) ~label #(do ~@body)))
 
