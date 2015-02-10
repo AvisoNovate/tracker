32c902cf3a8aec6dbe4635ec9583bafd10e924ac
Howard M. Lewis Ship <hlship@gmail.com>
2015-02-10 09:21:21 -0800

    Add some tools to make it easier to experiment

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,11 +1,30 @@
 (ns user
   (:use io.aviso.repl
+        io.aviso.logging
+        io.aviso.tracker
         clojure.pprint)
   (:require [clojure.tools.logging :as l]))
 
 (install-pretty-exceptions)
+(io.aviso.logging/install-pretty-logging)
+(io.aviso.logging/install-uncaught-exception-handler)
 
 (defmacro other-thread
   [& body]
-  `(.start (Thread. (bound-fn [] ~@body))))
+  `(.start (Thread. (bound-fn []
+                      (checkpoint
+                        ~@body)))))
 
+
+(defn demo-1
+  []
+  (track "on main thread" (other-thread (throw (RuntimeException. "hello")))))
+
+(defn demo-2
+  []
+  (track "outer" (track "middle - a" (other-thread (try
+                                                     (track "inner - a" (throw (ex-info "blowed up - a" {})))
+                                                     (catch Throwable _))))
+         (track "middle - b" (other-thread (try
+                                             (track "inner - b" (throw (ex-info "blowed up - b" {})))
+                                             (catch Throwable _))))))
